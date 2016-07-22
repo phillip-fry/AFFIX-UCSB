84d6c287b6866aabf01a8f2f913131a6f74e64f4
admin <phillip_fry@umail.ucsb.edu>
2016-07-22 16:30:45 -0700

    can't figure out mail

--- app/src/main/java/com/ucsbapp/phillip/affix/RegisterStudent.java
+++ app/src/main/java/com/ucsbapp/phillip/affix/RegisterStudent.java
@@ -1,48 +1,32 @@
 package com.ucsbapp.phillip.affix;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Message;
-import android.provider.ContactsContract;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.ucsbapp.phillip.affix.mail.GMailSender;
-import com.ucsbapp.phillip.affix.mail.SMTPMail;
-import com.ucsbapp.phillip.affix.mail.SendMail;
-import com.ucsbapp.phillip.affix.mail.mail;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Properties;
-import java.util.jar.Attributes;
-
+import javax.mail.Authenticator;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+
 
 public class RegisterStudent extends AppCompatActivity {
 
-    private mail M;
+    Session session = null;
+    ProgressDialog pdialog = null;
+    Context context = null;
+    EditText reciep;
+    String rec, subject, textMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,28 +49,31 @@ public class RegisterStudent extends AppCompatActivity {
         majors.setAdapter(majorAdapter);
 
         //Set the email account to send the confirmation email
-        M = new mail("phillipfry5@gmail.com", "fryball7");
+        reciep = (EditText) findViewById(R.id.umailpreffix);
 
 
     }
 
 
-    private void sendEmail(){
-        EditText edittextemail = (EditText)findViewById(R.id.umailpreffix);
+    private void sendEmail() {
+        EditText edittextemail = (EditText) findViewById(R.id.umailpreffix);
 
         //String emailSend = (edittextemail.getText().toString() + "@umail.ucsb.edu").trim();
         String emailSend = ("phillipfry5@gmail.com").trim();
         String subject = ("Test Sub").trim();
         String body = ("Testing").trim();
 
-        SendMail sm = new SendMail(this, emailSend, subject, body);
-
-        sm.execute();
     }
 
+    private class SMTPAuthenticator extends Authenticator
+    {
+        public PasswordAuthentication getPasswordAuthentication()
+        {
+            return new PasswordAuthentication("philfry125@gmail.com", "fryball7");
+        }
+    }
 
-
-    public void REGISTER(View v) {
+    public void REGISTER(View v) throws MessagingException {
         View view = LayoutInflater.from(RegisterStudent.this).inflate(R.layout.confcoderegister, null);
 
         //allows us to print at bottom of the register button
@@ -95,84 +82,27 @@ public class RegisterStudent extends AppCompatActivity {
         //allows us to get the user input in the umail section and add the @umail part
         final EditText emailprefix = (EditText) findViewById(R.id.umailpreffix);
         String emailpretext = emailprefix.getText().toString();
-        final String email = emailpretext + "@umail.ucsb.edu";
+        //final String email = emailpretext + "@umail.ucsb.edu";
+        final String email = "phillipfry5@gmail.com";
 
-        sendEmail();
-    }
+        //MailService mailService = new MailService();
+        //mailService.sendEmail();
 
-
-
-        /*
-        Properties props = new Properties();
-        props.put("mail.smtp.host","smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
-
-        Session session = Session.getDefaultInstance(props,new javax.mail.Authenticator(){
-            protected PasswordAuthentication getPasswordAuthentication(){
-                return new PasswordAuthentication("philfry125@gmail.com", "fryball7");
-            }
-        });
-        try{
-            MimeMessage message = new MimeMessage(session);
-            message.setSender(new InternetAddress("philfry125@gmail.com"));
-            message.addRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse("phillipfry5@gmail.com"));
-            message.setSubject("Test");
-            message.setContent("Hello to AFFIX","text/html; charset=utf-8");
-
-            Transport transport = session.getTransport("smtps");
-            transport.connect("philfry125@gmail.com", "Phillip Fry", "fryball7");
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
-            //Transport.connect();
-            //Transport.send(message);
-            //Transport.close();
-        }
-        catch (MessagingException e){
-            throw new RuntimeException(e);
-        }
-*/
-
-    //mail M = new mail("philfry125@gmail.com", "fryball7");
+        //Makes the popup dialog for the user to input the confirmation code
 /*
-        GMailSender sender = new GMailSender("philfry125@gmail.com","fryball7");
         try {
-            sender.sendMail("Email Subject", "<b>Hi</b><br/>Complete msg with html tags.", "philfry125@gmail.com", "phillipfry5@gmail.com", "phillip_fry@umail.ucsb.edu");
+            GMailSender sender = new GMailSender("philfry125@gmail.com",
+                    "<fryball7>");
+            sender.sendMail("This is Subject", "This is Body",
+                    "philfry125@gmail.com", "phillipfry5@gmail.com");
+            Toast.makeText(RegisterStudent.this, "Mail Send Successfully.....", Toast.LENGTH_LONG).show();
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //thread.start();
-*/
-/*
-        String TOArray[] = {email};
-        M.setTo(TOArray);
-        M.setFrom("phillipfry5@gmail.com");
-        M.setSubject("Welcome to AFFIX-UCSB");
-        M.setBody("You are the first user to receive an email. App created by Phillip Fry");
-
-
-        try {
-            M.send();
-            if (M.send()) {
-                // success
-                Toast.makeText(RegisterStudent.this, "Email was sent successfully.", Toast.LENGTH_LONG).show();
-            } else {
-                // failure
-                Toast.makeText(RegisterStudent.this, "Email was not sent.", Toast.LENGTH_LONG).show();
-            }
-        } catch (Exception e) {
-            // some other problem
-            Toast.makeText(RegisterStudent.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show();
+            Log.e("SendMail", e.getMessage(), e);
         }
 */
-
-    //Makes the popup dialog for the user to input the confirmation code
-        /*
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(RegisterStudent.this);
         alertBuilder.setView(view);
-        final EditText userInput = (EditText)view.findViewById(R.id.inputcode);
+        final EditText userInput = (EditText) view.findViewById(R.id.inputcode);
 
         alertBuilder.setCancelable(true).setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
             @Override
@@ -185,39 +115,7 @@ public class RegisterStudent extends AppCompatActivity {
         });
         Dialog dialog = alertBuilder.create();
         dialog.show();
-        */
 
-    /*
-
-    EditText firstname, lastname, umail, phoneNUM, password, confpassword;
-    Spinner major, residence;
-    String Firstname, Lastname, Umail, PhoneNUM, Password, Confpassword, Major, Residence;
-    Context ctx = this;
-
-    public void REGISTER(View v){
-        firstname = (EditText)findViewById(R.id.first_name);
-        lastname = (EditText)findViewById(R.id.last_name);
-        umail = (EditText)findViewById(R.id.umailpreffix);
-        phoneNUM = (EditText)findViewById(R.id.phone);
-        password = (EditText)findViewById(R.id.password);
-        confpassword = (EditText)findViewById(R.id.confirmpassword);
-        major = (Spinner)findViewById(R.id.MajorSpinner);
-        residence = (Spinner)findViewById(R.id.ResidenceSpinner);
-
-        Firstname = firstname.getText().toString();
-        Lastname = lastname.getText().toString();
-        Umail = umail.getText().toString() + "@umail.ucsb.edu";
-        PhoneNUM = phoneNUM.getText().toString();
-        Password = password.getText().toString();
-        Confpassword = confpassword.getText().toString();
-        Major = major.getSelectedItem().toString();
-        Residence = residence.getSelectedItem().toString();
-
-        Background b = new Background();
-        b.execute();
 
     }
-    */
 }
-
-
--- app/src/main/java/com/ucsbapp/phillip/affix/mail/Config.java
+++
@@ -1,9 +0,0 @@
-package com.ucsbapp.phillip.affix.mail;
-
-/**
- * Created by Phillip Fry on 7/22/2016.
- */
-public class Config {
-    public static final String EMAIL ="Phillip Fry";
-    public static final String PASSWORD ="fryball7";
-}
