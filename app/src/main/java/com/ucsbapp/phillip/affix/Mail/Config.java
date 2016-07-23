43a950ba3de2f95297c2a01a3b613bb2555258dd
phillip-fry <phillip_fry@umail.ucsb.edu>
2016-07-23 01:38:37 -0700

    Registration email works!

---
+++ app/src/main/java/com/ucsbapp/phillip/affix/Mail/Config.java
@@ -0,0 +1,9 @@
+package com.ucsbapp.phillip.affix.Mail;
+
+/**
+ * Created by Phillip Fry on 7/22/2016.
+ */
+public class Config {
+    public static final String EMAIL ="philfry125@gmail.com";
+    public static final String PASSWORD ="fryball7";
+}
--- app/src/main/java/com/ucsbapp/phillip/affix/RegisterStudent.java
+++ app/src/main/java/com/ucsbapp/phillip/affix/RegisterStudent.java
@@ -4,15 +4,22 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ucsbapp.phillip.affix.Mail.SendMail;
+
+import java.util.Random;
 
 import javax.mail.Authenticator;
 import javax.mail.MessagingException;
@@ -22,11 +29,9 @@ import javax.mail.Session;
 
 public class RegisterStudent extends AppCompatActivity {
 
-    Session session = null;
-    ProgressDialog pdialog = null;
-    Context context = null;
-    EditText reciep;
-    String rec, subject, textMessage;
+   private Button registerbtn;
+   private EditText reciep;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,29 +54,11 @@ public class RegisterStudent extends AppCompatActivity {
         majors.setAdapter(majorAdapter);
 
         //Set the email account to send the confirmation email
-        reciep = (EditText) findViewById(R.id.umailpreffix);
 
 
     }
 
 
-    private void sendEmail() {
-        EditText edittextemail = (EditText) findViewById(R.id.umailpreffix);
-
-        //String emailSend = (edittextemail.getText().toString() + "@umail.ucsb.edu").trim();
-        String emailSend = ("phillipfry5@gmail.com").trim();
-        String subject = ("Test Sub").trim();
-        String body = ("Testing").trim();
-
-    }
-
-    private class SMTPAuthenticator extends Authenticator
-    {
-        public PasswordAuthentication getPasswordAuthentication()
-        {
-            return new PasswordAuthentication("philfry125@gmail.com", "fryball7");
-        }
-    }
 
     public void REGISTER(View v) throws MessagingException {
         View view = LayoutInflater.from(RegisterStudent.this).inflate(R.layout.confcoderegister, null);
@@ -82,39 +69,71 @@ public class RegisterStudent extends AppCompatActivity {
         //allows us to get the user input in the umail section and add the @umail part
         final EditText emailprefix = (EditText) findViewById(R.id.umailpreffix);
         String emailpretext = emailprefix.getText().toString();
-        //final String email = emailpretext + "@umail.ucsb.edu";
-        final String email = "phillipfry5@gmail.com";
+        final String email = emailpretext + "@umail.ucsb.edu";
+        //final String email = "phillip_fry@umail.ucsb.edu";
 
-        //MailService mailService = new MailService();
-        //mailService.sendEmail();
+        Random var = new Random();
+        char one = (char)(var.nextInt(26) + 'a');
+        char two = (char)(var.nextInt(26) + 'a');
+        char three = (char)(var.nextInt(26) + 'a');
+        int first = var.nextInt(100) + 1;
+        int second = var.nextInt(100) + 1;
+        String firstnum = Integer.toString(first);
+        String secondnum = Integer.toString(second);
+        final String code = one + firstnum + two + secondnum + three;
+        String body = "Welcome to AFFIX-UCSB." + "\n" + "Your Confirmation Code is " +
+                code + "\n" + "\n" + "Created by Phillip Fry";
 
-        //Makes the popup dialog for the user to input the confirmation code
-/*
-        try {
-            GMailSender sender = new GMailSender("philfry125@gmail.com",
-                    "<fryball7>");
-            sender.sendMail("This is Subject", "This is Body",
-                    "philfry125@gmail.com", "phillipfry5@gmail.com");
-            Toast.makeText(RegisterStudent.this, "Mail Send Successfully.....", Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            Log.e("SendMail", e.getMessage(), e);
-        }
-*/
+        //Creating SendMail object
+        SendMail sm = new SendMail(this,email,"Welcome to AFFIX-UCSB", body);
+        //Executing sendmail to send mail
+        sm.execute();
+
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(RegisterStudent.this);
         alertBuilder.setView(view);
         final EditText userInput = (EditText) view.findViewById(R.id.inputcode);
 
-        alertBuilder.setCancelable(true).setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+
+        alertBuilder.setCancelable(false).setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                String usertext = userInput.getText().toString();
-                result.setText(email);
-
-
+                //Overwrite later
             }
         });
-        Dialog dialog = alertBuilder.create();
+
+        final AlertDialog dialog = alertBuilder.create();
         dialog.show();
+
+
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Boolean wantToCloseDialog = false;
+                String usertext = userInput.getText().toString();
+                if (usertext.equals(code)){
+                    Intent intent = new Intent(RegisterStudent.this, HomeScreen.class);
+                    startActivity(intent);
+                    wantToCloseDialog = true;
+                } else {
+                    Toast.makeText(getApplicationContext(),"Incorrect Code",Toast.LENGTH_LONG).show();
+                }
+                if(wantToCloseDialog)
+                    dialog.dismiss();
+            }
+        });
+
+
+        alertBuilder.setCancelable(false).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               dialog.dismiss();
+            }
+        });
+
+
+
+
+
 
 
     }
